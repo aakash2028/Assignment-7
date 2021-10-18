553831c0a38e31a16da91c96ceadb78b9d75cffb
aakash2028 <ajoshi3054@sdsu.edu>
2021-10-17 23:38:10 -0700

    Change in event listener file

--- Practice with Event Model/js/script.js
+++ Practice with Event Model/js/script.js
@@ -5,25 +5,33 @@ function displayMessage() {
 }
 
 // Element Property
-var myElement = document.getElementById("myButton")
-myElement.onclick = function () {
-    "use strict";
-    window.alert("I have been clicked.. (Element Property)");
+window.onload = function () {
+    var myElement = document.getElementById("myButton")
+        myElement.onclick = function () {
+            window.alert("I have been clicked.. (Element Property)");
+    }
 }
+
 
 // User Defined
 function btnOnClick() {
     "use strict";
-    window.alert("I have been clicked..(User Defined Function)")
+    return window.alert("I have been clicked (STEP 3) User Defined Function");
 }
-var myOtherButton = window.document.getElementById("myOtherButton")
-myOtherButton.addEventListener("click", btnOnClick);
 
-// Call-Back Function
-var myOtherButton2 = window.document.getElementById("myOtherButton2")
-myOtherButton2.addEventListener("click", function() {
+window.addEventListener("load", function () {
     "use strict";
-    window.alert("I have been clicked..(Call Back Function)")
+    var myOtherButton = window.document.getElementById("myOtherButton");
+    myOtherButton.addEventListener("click", btnOnClick);
 });
 
-// Event Listners
+
+// Call-Back Function
+
+window.addEventListener("load", function () {
+    "use strict";
+    var myOtherButton2 = window.document.getElementById("myOtherButton2");
+    myOtherButton2.addEventListener("click", function () {
+        return window.alert("I have been clicked ..(Call Back Function)");
+    });
+});
